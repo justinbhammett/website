c0db711b520f4e2ea1dbdad4070dfdfa6942884b
justinbhammett <justinbhammett@gmail.com>
2024-11-04 15:29:17 -0800

    added leafNode, parentNode, textNode, convert textNode to HtmlNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,4 @@
+from textnode import *
 
 class HTMLNode():
     def __init__(self, tag=None, value=None, children=None, props=None):
@@ -22,10 +23,79 @@ class HTMLNode():
         return False
     def __repr__(self):
         return f"{' '.join(f'{key}={value}' for key, value in self.__dict__.items())}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("no value")
+        if self.tag is None:
+            return self.value
+        if self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        # prop_iteration = f"{' '.join(f'{key}="{value}"' for key, value in self.props.items())}"
+        prop_iteration = self.props_to_html()
+        return f"<{self.tag} {prop_iteration}>{self.value}</{self.tag}>"
     
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props=None)
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag")
+        if self.children is None:
+            raise ValueError("no children")
+        if self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        child_iteration = f"{' '.join(f'{kid.to_html()}' for kid in self.children)}"
+        # prop_iteration = f"{' '.join(f'{key}="{value}"' for key, value in self.props.items())}"
+        prop_iteration = self.props_to_html()
+        return f"<{self.tag} {prop_iteration}>{child_iteration}</{self.tag}>"
+   
+    def __repr__(self):
+            return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+        
+def text_node_to_html_node(TextNode):
+    match TextNode.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=TextNode.text, props=None)
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=TextNode.text, props=None)
+        case TextType.ITALIC:
+            return LeafNode(tag="i", value=TextNode.text, props=None)
+        case TextType.CODE:
+            return LeafNode(tag="code", value=TextNode.text, props=None)
+        case TextType.LINK:
+            return LeafNode(tag="a", value=TextNode.text, props={"href": TextNode.url})
+        case TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": TextNode.url, "alt": TextNode.text})
+
+    raise Exception("Invalid type")
+        
+
+
+
 def main():
     text1 = HTMLNode(tag="p", value=None, children=None, props={"href": "http://www.google.com", "target": "_blank"})
-    print(text1.props_to_html())
+    # print(text1.props_to_html())
+
+    leaf1 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    # print(leaf1.to_html())
+
+    parent1 = ParentNode("a", [LeafNode("p", "Click me!"), LeafNode("p", "Don't do it!")], {"href": "https://www.amazon.com"})
+    # print(parent1.to_html())
+
+    text2 = TextNode("What is this?", TextType.BOLD, "https://www.boot.dev")
+    print(text_node_to_html_node(text2).to_html())
 
 main()
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
+from textnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -21,6 +22,39 @@ class TestHTMLNode(unittest.TestCase):
         node2 = HTMLNode(tag=None, value=None, children=None, props=None)
         self.assertNotEqual(node, node2)
 
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        node = LeafNode(tag="p", value="what?", props={"href": "http://www.google.com"})
+        node2 = LeafNode(tag="p", value="what?", props={"href": "http://www.google.com"})
+        self.assertEqual(node, node2)
+    def test_eq1(self):
+        node = LeafNode(tag="hr", value=None, props=None)
+        node2 = LeafNode(tag=None, value=None, props=None)
+        self.assertNotEqual(node, node2)
+    def test_eq2(self):
+        node = LeafNode(tag=None, value=None, props=None)
+        node2 = LeafNode(tag=None, value=None,  props=None)
+        self.assertEqual(node, node2)
+    def test_eq3(self):
+        node = LeafNode(tag=None, value="What?", props=None)
+        node2 = LeafNode(tag=None, value=None, props=None)
+        self.assertNotEqual(node, node2)
+
+class TestParentNode(unittest.TestCase):
+    def test_eq(self):
+        node = ParentNode(tag="a", children=[LeafNode(tag="p", value="what?"), LeafNode(tag="i", value="italic text")], props={"href": "http://www.google.com"})
+        node2 = ParentNode(tag="a", children=[LeafNode(tag="p", value="what?"), LeafNode(tag="i", value="italic text")], props={"href": "http://www.google.com"})
+        self.assertEqual(node, node2)
+    def test_eq1(self):
+        node = ParentNode(tag="p", children=[ParentNode(tag="a", children=[LeafNode(tag="p", value="what?"), LeafNode(tag="i", value="italic text")], props={"href": "http://www.google.com"})], props=None)
+        node2 = ParentNode(tag="p", children=[ParentNode(tag="a", children=[LeafNode(tag="p", value="what?"), LeafNode(tag="i", value="italic text")], props={"href": "http://www.google.com"})], props=None)
+        self.assertEqual(node, node2)
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        textnode1 = TextNode(text="picture of elephant", text_type=TextType.LINK, url="http://www.elephant.com")
+        textnode2 = TextNode(text="picture of elephant", text_type=TextType.LINK, url="http://www.elephant.com")
+        self.assertEqual(textnode1, textnode2)
 
 if __name__ == "__main__":
     unittest.main()
